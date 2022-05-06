6daafd633de8cb233dcdb2ef1e6ea6d1ab3b6a3f
Luis <rluis747@gmail.com>
2022-05-05 19:40:19 -0500

    Ready header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import "./Header.scss";
 import { images } from "../../constants";
-import React from "react";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { BiCaretRightCircle } from "react-icons/bi";
+import { IoIosStar } from "react-icons/io";
 
 const Header = () => {
   return (
@@ -9,13 +11,51 @@ const Header = () => {
         <img src={images.perruless} alt="" />
         <div className="app__container-info">
           <div className="phone">
-            <span>1-415-800-4781</span>
+            <BsFillTelephoneFill className="icon" size={10} />
+            <div>1-415-800-4781</div>
           </div>
           <select className="versions" name="Version" id="version">
             <option value="Version 1">Version 1</option>
             <option value="Version 2">Version 2</option>
             <option value="Version 3">Version 3</option>
           </select>
+        </div>
+        <div className="app__header-greetins">
+          <span>Hello Luis</span>
+          <p>
+            My name is Ryle,
+            <br />
+            I'll be your personal travel advisor.
+          </p>
+          <span>Let's start planning!</span>
+          <div className="app__header-linkwhyus">
+            <a href="http://">Why us?</a>
+            <BiCaretRightCircle size={15} />
+          </div>
+
+          <div className="app__header-awards">
+            <div className="awards">
+              Ranked <span>#1</span> <br />
+              in the word
+            </div>
+            <div className="trustpilot">
+              <IoIosStar />
+              Trustpilot
+            </div>
+          </div>
+        </div>
+        <div className="app__header-advisor-info">
+          <figure>
+            <img src={images.advisor} alt="" />
+          </figure>
+          <div className="app__header-advisor" >
+            <span>Rylee McGowan</span>
+            <div className="phoneAdvisor">
+              <BsFillTelephoneFill className="icon" size={10} />
+              <div>1-415-800-4781</div>
+            </div>
+            <a className="buttonReadme" href="#">Read more</a>
+          </div>
         </div>
       </div>
     </div>
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,11 @@
 import perruless from "../assets/perruless.png";
+import arrow from "../assets/arrow.svg"
+import advisor from "../assets/advisor.png"
 
 const images ={
-    perruless
+    perruless,
+    arrow,
+    advisor
 }
 
 export default images
